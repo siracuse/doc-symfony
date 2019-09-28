5d6e133377940183db5959ccc0f6e0f5ebb2b5ba
siracuse <siracuse.harichandra@gmail.com>
2019-09-28 16:25:16 +0300

    creation et remplissage de la page accueil.php

--- index.php
+++ index.php
@@ -9,81 +9,30 @@
 <body>
     <?php include('menu.php'); ?>
 
-    <form action="" method="post" class="onetomany">
-        <input type="text" name="firstname" class="input-firstname" id="firstname" required>
-        <label for="firstname" class="label-firstname">0,n</label>--------------
-        <label for="secondname" class="label-secondname">1,1</label>
-        <input type="text" name="secondname" class="input-secondname" id="secondname" required>
+    <h1>Documentation Symfony 3.4</h1>
 
-        <input type="submit" value="Valider" name="onetomany">
-    </form>
-
-    <?php
-
-if (isset($_POST['onetomany'])) {
-    $firstname = ucfirst($_POST['firstname']);
-    $secondname = ucfirst($_POST['secondname']);
-
-    $response = '<h3>Relation One-To-Many - Bidirectional </h3>';
-    $response .= '<div class="code"><code>class '.$firstname;
-    $response .= '<br> {';
-    $response .= '<br> /**';
-    $response .= '<br> * @ORM\OneToMany(targetEntity="'.$secondname.'", mappedBy="'.lcfirst($firstname).'"';
-    $response .= '<br> */';
-    $response .= '<br> private $'.lcfirst($secondname).'s;</code></div>';
-
-    $response .= '<br><hr><br>';
-    $response .= '<div class="code lastcode"><code>class '.$secondname;
-    $response .= '<br> {';
-    $response .= '<br> /**';
-    $response .= '<br> * @ORM\ManyToOne(targetEntity="'.$firstname.'", inversedBy="'.lcfirst($secondname).'s"';
-    $response .= '<br> * @ORM\JoinColumn(name="'.lcfirst($firstname).'_id", referencedColumnName="id"';
-    $response .= '<br> */';
-    $response .= '<br> private $'.lcfirst($firstname).';</code></div>';
-    echo $response;
-    unset($_POST);
-}
-
-?>
+    <h2>Le but de ce site</h2>
+    <p>J'ai créé ce site web afin de regrouper des mémos, des morceaux de code, des solutions et des outils
+    dans le but de réaliser mes projets symfony plus facilement et rapidement </p>
 
 
-<form action="" method="post" class="form-example">
-        <input type="text" name="firstname" class="input-firstname" id="firstname" required>
-        <label for="firstname" class="label-firstname">0,n</label>--------------
-        <label for="secondname" class="label-secondname">0,n</label>
-        <input type="text" name="secondname" class="input-secondname" id="secondname" required>
+    <h2><a href="cmd.php">CMD Utile</a> - Mes commandes les plus utilées</h2>
+    <p>Sur cette page, vous retrouverez une liste de commande terminal que j'utilise le plus souvent.
+    Cela peut être la création d'un bundle à la génération des getters et setters. </p>
 
-        <input type="submit" value="Valider" name="manytomany">
-</form>
+    <h2>Formulaire de génération d'annotation relative au relation entité</h2>
+    <p>Sur ces pages, vous retrouverez plusieurs formulaires permettant de générer les annotations automatiquement.<br>
+    Par exemple pour une relation OneToMany entre un "Produit" et une "Marque"
+    (Une marque peut avoir plusieurs produits, mais un produit n'est associé qu'a une seule marque), on obtient :
 
-<?php
+    <div class="code"><code>class Marque<br> {<br> /**<br> * @ORM\OneToMany(targetEntity="Produit", mappedBy="marque")<br> */<br> private $produits;</code></div>
+    <br>
+    <div class="code lastcode"><code>class Produit<br> {<br> /**<br> * @ORM\ManyToOne(targetEntity="Marque", inversedBy="produits")<br> * @ORM\JoinColumn(name="marque_id", referencedColumnName="id")<br> */<br> private $marque;</code></div>
+    </p>
 
-if (isset($_POST['manytomany'])) {
-    $firstname = ucfirst($_POST['firstname']);
-    $secondname = ucfirst($_POST['secondname']);
 
-    $response = '<h3>Relation Many-To-Many - Bidirectional </h3>';
-    $response .= '<div class="code"><code>class '.$firstname;
-    $response .= '<br> {';
-    $response .= '<br> /**';
-    $response .= '<br> * @ORM\ManyToMany(targetEntity="'.$secondname.'", inversedBy="'.lcfirst($firstname).'s"';
-    $response .= '<br> * @ORM\JoinTable(name="'.lcfirst($firstname).'s_'.lcfirst($secondname).'s"';
-    $response .= '<br> */';
-    $response .= '<br> private $'.lcfirst($secondname).'s;</code></div>';
-
-    $response .= '<br><hr><br>';
-    $response .= '<div class="code lastcode"><code>class '.$secondname;
-    $response .= '<br> {';
-    $response .= '<br> /**';
-    $response .= '<br> * @ORM\ManyToMany(targetEntity="'.$firstname.'", mappedBy="'.lcfirst($secondname).'s"';
-    $response .= '<br> */';
-    $response .= '<br> private $'.lcfirst($firstname).'s;</code></div>';
-    echo $response;
-    unset($_POST);
-}
-
-?>
-
+    <h2>Source</h2>
+    <p>Documentation officiel de symfony (anglais) : <a href="https://symfony.com/doc/3.4//index.html#gsc.tab=0">symfony.com/doc/3.4</a> </p>
 </body>
 
 </html>
