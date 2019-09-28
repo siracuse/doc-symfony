6c7348c9598ab53003b0aedf792403df22db5127
siracuse <siracuse.harichandra@gmail.com>
2019-09-28 15:55:47 +0300

    creation d'une classe CMD pour la page cmd.php

--- class/Cmd.php
+++ class/Cmd.php
@@ -1,18 +1,29 @@
 <?php
 
-class Cmd {
+class Cmd
+{
 
     private $title;
     private $cmd;
+    private $description;
 
-    public function __construct ($title, $cmd) {
+    public function __construct ($title, $cmd, $description = null)
+    {
         $this->title = $title;
         $this->cmd = $cmd;
+        $this->description = $description;
     }
 
-    public function getResponse() {
-        $response = '<div class="title-cmd">'.$this->title.'</div>';
+    public function getResponse()
+    {
+        $response = '<div class="div-cmd">';
+        $response .= '<div class="title-cmd">'.$this->title.'</div>';
+        // if (isset($this->description)) {
+        //     $response .= '# ';
+        // }
         $response .= '<div class="code"><code>'.$this->cmd.'</code></div>';
+        $response .= '</div>';
+
         return $response;
     }
 }
--- cmd.php
+++ cmd.php
@@ -13,16 +13,49 @@ include('class/Cmd.php');
 <body>
     <?php include('menu.php'); ?>
 
+    <h1>Les CMD utiles</h1>
+
     <?php
-    $cmd = new Cmd('Nouveau projet Symfony 3', '>composer create-project symfony/framework-standard-edition project-name');
+    $cmd = new Cmd('Nouveau projet Symfony 3', 'composer create-project symfony/framework-standard-edition project-name');
     echo ($cmd->getResponse());
+
+    $cmd = new Cmd('Création d\'un bundle', 'php bin/console generate:bundle');
+    echo ($cmd->getResponse());
+
+    echo '<hr>';
+
+    $cmd = new Cmd('Création de la bdd', 'php bin/console doctrine:database:create');
+    echo ($cmd->getResponse());
+
+    $cmd = new Cmd('Configuration de la bdd',
+    '# app/config/config.yml<br>
+    doctrine:<br>
+    &nbsp;dbal:<br>
+    &nbsp;&nbsp;driver:   pdo_mysql<br>
+    &nbsp;&nbsp;host:     "%database_host%"<br>
+    &nbsp;&nbsp;dbname:   "%database_name%"<br>
+    &nbsp;&nbsp;user:     "%database_user%"<br>
+    &nbsp;&nbsp;password: "%database_password%"<br>'
+    );
+    echo ($cmd->getResponse());
+
+    echo '<hr>';
+
+    $cmd = new Cmd('Création d\'une entité', 'php bin/console doctrine:generate:entity');
+    echo ($cmd->getResponse());
+
+    $cmd = new Cmd('Générer les getters et les setters', 'php bin/console doctrine:generate:entities BundleName:entity_name');
+    echo ($cmd->getResponse());
+
+    $cmd = new Cmd('Visualiser les requetes qui vont être effectuées', 'php bin/console doctrine:schema:update --dump-sql');
+    echo ($cmd->getResponse());
+
+    $cmd = new Cmd('Exécuter les requêtes affichées précédemment', 'php bin/console doctrine:schema:update --force');
+    echo ($cmd->getResponse());
+
+
     ?>
-    <!-- Nouveau projet Symfony 3
-    <div class="code">
-        <code>
-            >create-project symfony/framework-standard-edition project_name
-        </code>
-    </div> -->
+
 
 
 
